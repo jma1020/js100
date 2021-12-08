a6e32a54c8bd7f66f08e08c535f66678eab8047b
jeongmin Kim <jma1020@naver.com>
2021-12-09 02:46:50 +0900

    js100day8

---
+++ 013.js
@@ -0,0 +1,7 @@
+const one = ["수성","금성","지구","화성","목성","토성","천왕성","해왕성"]
+
+
+
+const num = prompt("골라골라~");
+
+console.log(one[num-1]);
---
+++ 017.js
@@ -0,0 +1,7 @@
+const height = 160
+//const height = prompt("키 입력 구라치지마")
+if(height >= 150) {
+    console.log('yes')
+} else {
+    console.log('no')
+}
