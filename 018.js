a5163be8f3a9fd1f70f9042dd7568fe5d9962cb7
jeongmin Kim <jma1020@naver.com>
2021-12-09 22:14:29 +0900

    js100day9

--- 018.js
+++ 018.js
@@ -1,2 +1,13 @@
-const score = [12,6,9]
 
+const a =prompt("입력")
+const b =prompt("2입력")
+const c = prompt("3입력")
+
+console.log(a)
+const score = [parseInt(a,10),parseInt(b,10),parseInt(c,10)]
+
+let sum = 0;
+
+score.forEach(e=> sum+=e)
+
+sum/score.length
